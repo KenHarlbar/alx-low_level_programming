d537ddec4609595c94d49124544c7d74fd5d12ec
KenHarlbar <balotech514@gmail.com>
2022-03-16 09:22:53 +0100

    Consider negative numbers too and use _putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,14 @@
  */
 int print_last_digit(int n)
 {
-	int lastDigit = n % 10;
+	int lastDigit;
+
+	lastDigit = n % 10;
+	if (lastDigit < 0)
+	{
+		_putchar(-lastDigit + 48);
+		return (-lastDigit);
+	}
+	_putchar(lastDigit + 48);
 	return (lastDigit);
 }
