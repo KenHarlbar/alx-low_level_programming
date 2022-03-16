9ebf51eee5e56e0f37653c3abe9d5d9e2f1303db
KenHarlbar <balotech514@gmail.com>
2022-03-16 07:26:38 +0100

    RE-Initialized git

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, and then a newline
+ * @i: for iteration
+ * main - call the function print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+
+int main(void)
+{
+	print_alphabet();
+	return (0)
+}
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		++i;
+	}
+}
