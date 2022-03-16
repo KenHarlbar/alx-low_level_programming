4a00c2f1af8d4fcb582dd3bb0c0e9ff5339a9b69
KenHarlbar <balotech514@gmail.com>
2022-03-16 07:45:47 +0100

    Add description to the function 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, and then a newline
+ * print_alphabet_x10 - Prints 10x the alphabets in lowercase, and then a newline
  *
  * Return: Always 0 (Success)
  */
