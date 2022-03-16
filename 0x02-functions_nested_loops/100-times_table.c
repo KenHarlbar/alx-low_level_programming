521ca507d79b79eb9ff8d6c1eadc88ad5d7adfc7
KenHarlbar <balotech514@gmail.com>
2022-03-16 12:45:28 +0100

    Restructure :)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table
+ * print_times_table - Prints the n times table
+ *
+ * @n: number times table (0 < n <= 15)
  *
  * Return: no return
  */
@@ -9,38 +11,36 @@ void print_times_table(int n)
 {
 	int a, b, op;
 
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
-		return (0);
-	}
-
-	for (a = 0; a <= n; a++)
-	{
-		_putchar(48);
-		for (b = 1; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			op = a * b;
-			_putchar(44);
-			_putchar(32);
-			if (op <= 9)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
+				op = a * b;
+				_putchar(44);
 				_putchar(32);
-				_putchar(32);
-				_putchar(op + 48);
+				if (op <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
-			else if (op <= 99)
-			{
-				_putchar(32);
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
-			}
-			else
-			{
-				_putchar(((op / 100) % 10) + 48);
-				_putchar(((op / 10) % 10) + 48);
-				_putchar((op % 10) + 48);
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
