213b44e5c2d1bdf4d144d64bf686c30c20386b59
KenHarlbar <balotech514@gmail.com>
2022-03-16 07:38:06 +0100

    Removed main from the program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,9 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase, and then a newline
  * @i: for iteration
- * main - call the function print_alphabet
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-
-int main(void)
-{
-	print_alphabet();
-	return (0)
-}
-
 void print_alphabet(void)
 {
 	int i = 97;
@@ -24,4 +15,5 @@ void print_alphabet(void)
 		_putchar(i);
 		++i;
 	}
+	_putchar('\n');
 }
