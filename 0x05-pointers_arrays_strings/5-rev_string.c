f6dbb000ad7b8cc4314dcecaedcff2ba8da91b76
KenHarlbar <balotech514@gmail.com>
2022-03-22 10:02:56 +0100

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - print the reverse of a string
+ *
+ * @s: Input string
+ *
+ * Return: no return value
+ */
+void rev_string(char *s)
+{
+	int count = 0, countDown;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	countDown = count - 1;
+
+	while (countDown >= 0)
+	{
+		_putchar(*(s + countDown));
+		countDown--;
+	}
+}
