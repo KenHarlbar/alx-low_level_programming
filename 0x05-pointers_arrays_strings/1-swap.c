4c45b264a07844070d175b7c4080bb9f348937df
KenHarlbar <balotech514@gmail.com>
2022-03-22 08:26:31 +0100

    Include description for variables a and b

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,15 @@
 /**
  * swap_int - swap two numbers
  *
- * @swap: A holder
+ * @a: first variable
+ * @b: second variable
  *
  * Return: no return value
  */
 void swap_int(int *a, int *b)
 {
 	int swap;
+
 	swap = *a;
 	*a = *b;
 	*b = swap;
