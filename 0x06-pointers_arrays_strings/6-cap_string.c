80df242963b97b390b841f9d89f2f5dee125553f
KenHarlbar <balotech514@gmail.com>
2022-03-23 14:43:21 +0100

    Capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize a string
+ *
+ * @s: Input string
+ *
+ * Return: return the string with the first
+ * letters of all words capitalized
+ */
+char *cap_string(char *s)
+{
+	int count = 0, i = 0, sep_of_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+	{
+		*(s + count) = *(s + count) - 32;
+	}
+	while (*(s + count) != '\0')
+	{
+		while (i < 13)
+		{
+			if (*(s + (count + 1)) >= 97 && *(s + (count + 1)) <= 122)
+			{
+				*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+			i++;
+		}
+		count++;
+	}
+	return (s);
+}
