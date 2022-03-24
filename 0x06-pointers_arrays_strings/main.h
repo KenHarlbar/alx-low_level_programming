564326ea81020c8648b54f1c7450944813bb93e1
KenHarlbar <balotech514@gmail.com>
2022-03-24 16:41:53 +0100

    Wahala

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef _main_h_
 #define _main_h_
 
-  char *_strcat(char *dest, char *src);
+ char *_strcat(char *dest, char *src);
  char *_strncat(char *dest, char *src, int n);
+ char *cap_string(char *);
 
 #endif
