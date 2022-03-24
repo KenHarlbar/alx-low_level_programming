3cac268b22e98ed911ac92c1774eff6a1fc40ed9
KenHarlbar <balotech514@gmail.com>
2022-03-24 17:38:43 +0100

    Shit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,13 +19,13 @@ char *_strcat(char *dest, char *src)
 	while(count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
-		if (*(src + count2) != '\0')
+		if (*(src + count2) == '\0')
 		{
 			break;
 		}
 		count++;
 		count2++;
 	}
-	*(dest + count) = '\0';
+	dest[count] = '\0';
 	return (dest);
 }
