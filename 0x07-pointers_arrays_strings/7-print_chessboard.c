19eb8a7409be6cb7e25fa64b33609c56ffa1ad63
KenHarlbar <balotech514@gmail.com>
2022-03-29 00:35:25 +0100

    Add something light

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,15 +9,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	unsigned int i, m;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(*(a + j));
+			m = i;
+			_putchar('\n');
 		}
-		_putchar('\n');
+		_putchar(*(*(a + (i / 8))) + (i - m));
 	}
 	return (0);
 }
