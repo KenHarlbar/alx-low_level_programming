dd8d1f7a81bc682bd115433169fbabd4e832d8dc
KenHarlbar <balotech514@gmail.com>
2022-03-28 21:55:42 +0100

    Add description

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-/**********/
+/**
+ * _strchr - Locate a character in a string
+ *
+ * @s: input string
+ * @c: character to be located
+ *
+ * Return: pointer to the first occurrence
+ * of 'c' if available or NULL if not
+ */
 char *_strchr(char *s, char c)
 {
 	unsigned int count = 0;
