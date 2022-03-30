25f20f3b1c8b85f945c0dbd4e50c288cea48d4f4
KenHarlbar <balotech514@gmail.com>
2022-03-30 17:23:32 +0100

    Return the factorial of any given integer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * factorial - return the factorial of
+ * any given integer
+ *
+ * @n: input integer
+ *
+ * Return: 1 if n is 0 or 1
+ * -1 if n < 0 and n! if the given conditions
+ * are not met
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return n * factorial(n - 1);
+	}
+}
