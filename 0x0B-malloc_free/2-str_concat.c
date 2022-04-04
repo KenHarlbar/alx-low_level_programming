058f84cb450f21c256d1f92ea74989632a9f384d
KenHarlbar <balotech514@gmail.com>
2022-04-04 15:50:47 +0100

    Hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,8 +26,10 @@ char *str_concat(char *s1, char *s2)
 	count = count1 + count2;
 	strout = (char *)malloc(sizeof(char) * (count + 1));
 	if (strout == NULL)
+	{
 		free(strout);
 		return (NULL);
+	}
 	if (strout == NULL)
 		return (NULL);
 	for (j = 0; j < count1; j++)
