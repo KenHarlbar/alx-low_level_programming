45c75ef7862e373fb2d20b9c891159129038eea4
KenHarlbar <98645847+KenHarlbar@users.noreply.github.com>
2022-04-09 15:40:34 +0100

    Revamp

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,51 +2,48 @@
 #include <stdlib.h>
 
 /**
-  * string_nconcat - concatenate two strings
-  *
-  * @s1: first input string
-  * @s2: second input string
-  * @n: number of bytes
-  *
-  * Return: NULL if malloc fails and
-  * a pointer to s if otherwise
-  */
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int count, count1, count2, i;
-        char *s;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-        if (s1 == NULL)
-            s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-        if (s2 == NULL)
-            s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        count1 = 0;
-        while (s1[count1] != '\0')
-            count1++;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-        count2 = 0;
-        while (s2[count2] != '\0')
-            count2++;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-        if (n < count2)
-            count2 = n;
+	if (n > ls2)
+		n = ls2;
 
-        count = count1 + count2;
+	lsout = ls1 + n;
 
-        s = malloc(count + 1);
+	sout = malloc(lsout + 1);
 
-        if (s == NULL)
-            return (NULL);
+	if (sout == NULL)
+		return (NULL);
 
-        for (i = 0; i < count; i++)
-        {
-            if (i < count1)
-                s[i] = s1[i];
-            else
-                s[i] = s2[i - count1];
-        }
-        s[i] = '\0';
-        return (s);
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
