2ecf906055b627d3eebcd94eb6d255f779e76204
KenHarlbar <98645847+KenHarlbar@users.noreply.github.com>
2022-04-09 15:20:13 +0100

    Hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,32 +13,32 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int count, count1, count2, i;
-    char *s;
+        unsigned int count, count1, count2, i;
+        char *s;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    count1 = 0;
-    while (s1[count1] != '\0')
-        count1++;
-    count2 = 0;
-    while (s2[count2] != '\0')
-        count2++;
-    if (n < count2)
-        count2 = n;
-    count = count1 + count2;
-    s = malloc(count + 1);
-    if (s == NULL)
-        return (NULL);
-    for (i = 0; i < count; i++)
-    {
-        if (i < count1)
-            s[i] = s1[i];
-        else
-            s[i] = s2[i - count1];
-    }
-    s[i] = '\0';
-    return (s);
+        if (s1 == NULL)
+            s1 = "";
+        if (s2 == NULL)
+            s2 = "";
+        count1 = 0;
+        while (s1[count1] != '\0')
+            count1++;
+        count2 = 0;
+        while (s2[count2] != '\0')
+            count2++;
+        if (n < count2)
+            count2 = n;
+        count = count1 + count2;
+        s = malloc(count + 1);
+        if (s == NULL)
+            return (NULL);
+        for (i = 0; i < count; i++)
+        {
+            if (i < count1)
+                s[i] = s1[i];
+            else
+                s[i] = s2[i - count1];
+        }
+        s[i] = '\0';
+        return (s);
 }
