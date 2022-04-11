288d9e937eb8f2a4d5933175ba2005139e487111
KenHarlbar <balotech514@gmail.com>
2022-04-11 16:59:13 +0100

    Hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * inti_dog - initialize a variable of type struct dog
+ * init_dog - initialize a variable of type struct dog
  *
  * @d: passed variable for the type
  * @name: name of the dog
