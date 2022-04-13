795ac475f79342f3505da9b523aa82f6ed7f8a61
KenHarlbar <balotech514@gmail.com>
2022-04-13 22:48:52 +0100

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - print a name
+ *
+ * @name: name to be printed
+ * @f: pointer to function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
