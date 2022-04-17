b9dfd37f63f5a5318199bd4a9b0566acec2dc746
KenHarlbar <balotech514@gmail.com>
2022-04-17 11:57:55 +0100

    Add description for betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,14 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
-/********/
+/**
+ * print_numbers - print numbers, followed by a new line
+ *
+ * @separator: string to be printed after each parameter
+ * @n: number of parameters
+ *
+ * Return: nothing
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
