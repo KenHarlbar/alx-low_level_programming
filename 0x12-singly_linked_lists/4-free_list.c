5a4ce2efed25c92aa9001142fa4c707888042443
KenHarlbar <balotech514@gmail.com>
2022-04-21 22:18:43 +0100

    Free a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: top node
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		list_t *temp = head;
+		
+		head = temp->next;
+		free(temp);
+	}
+}
