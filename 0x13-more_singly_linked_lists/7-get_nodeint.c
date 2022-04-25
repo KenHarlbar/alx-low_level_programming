ee9235d2f36753326687a7fdc5e1b93375874a7b
KenHarlbar <balotech514@gmail.com>
2022-04-25 22:32:54 +0100

    Hot fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -22,7 +22,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		listint_t *temp = head;
 
 		for (i = 0; i < index; i++)
+		{
 			temp = temp->next;
+			if (temp == NULL)
+				return (NULL);
+		}
 		return (temp);
 	}
 }
