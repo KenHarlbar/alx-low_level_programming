b7d29da21436222828a6b059c2193de2c3d17d71
baaioy <balotech514@gmail.com>
2022-07-02 23:43:43 +0100

    Hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,7 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = prev = top = ht->array[index];
 		while (current)
 		{
-			if (!(strcmp(current->key, new_node->key)) && (i = 0))
+			if (!(strcmp(current->key, new_node->key)) && (i == 0))
 			{
 				new_node->next = current->next;
 				free_node(current);
